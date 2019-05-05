d3c11ffcd13ff625ca5393cb629592b48b6132b6
CelineDknp <celine.deknop@student.uclouvain.be>
2019-05-05 13:27:44 +0200

    Trying stuff

--- test.c
+++ test.c
@@ -19,19 +19,17 @@ PROCESS_THREAD(timer_process, ev, data)
 	printf("++++++++++++++++++++++++++++++\n");
  
     while(1) {
-	/* Do the rest of the stuff here. */
- 
-	static uint32_t ticks = 0;
-	static uint32_t seconds = 5;
-	static struct etimer et; // Define the timer
-	etimer_set(&et, CLOCK_SECOND*seconds);  // Set the timer
- 
-	PROCESS_WAIT_EVENT();  // Waiting for a event, don't care which
- 
-	if(etimer_expired(&et)) {  // If the event it's provoked by the timer expiration, then...
-		printf("Timer has expired !");
-	}
-			
+		/* Do the rest of the stuff here. */
+		static uint32_t seconds = 5;
+		static struct etimer et; // Define the timer
+		etimer_set(&et, CLOCK_SECOND*seconds);  // Set the timer
+	 
+		PROCESS_WAIT_EVENT();  // Waiting for a event, don't care which
+	 
+		if(etimer_expired(&et)) {  // If the event it's provoked by the timer expiration, then...
+			printf("Timer has expired !");
+		}
+	}	
 	exit:
 		PROCESS_END();
 }
