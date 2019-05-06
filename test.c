c945ec9f05f77adbdb884a1bbd5615782b1a08bb
CelineDknp <celine.deknop@student.uclouvain.be>
2019-05-06 08:47:13 +0200

    Broadcasting

--- test.c
+++ test.c
@@ -1,35 +1,42 @@
 #include "contiki.h"
 #include <stdio.h> /* For printf() */
+#include "net/rime.h"
+#include "random.h"
  
 //-----------------------------------------------------------------   
 PROCESS(timer_process, "timer with print example");
 AUTOSTART_PROCESSES(&timer_process);
 //-----------------------------------------------------------------
- 
+// Functions
+static void broadcast_recv(struct broadcast_conn *c, const rimeaddr_t *from)
+{
+  printf("broadcast message received from %d.%d: '%s'\n",
+         from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
+}
+static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
+static struct broadcast_conn broadcast;
  
 PROCESS_THREAD(timer_process, ev, data)
 {   
-   PROCESS_EXITHANDLER(goto exit);
-   PROCESS_BEGIN();
- 
-  /* Initialize stuff here. */ 
- 
-	printf("\n++++++++++++++++++++++++++++++\n");
-	printf("+    		HELLO WORDL		   +\n");
-	printf("++++++++++++++++++++++++++++++\n");
- 
-    while(1) {
-		/* Do the rest of the stuff here. */
-		static uint32_t seconds = 5;
-		static struct etimer et; // Define the timer
-		etimer_set(&et, CLOCK_SECOND*seconds);  // Set the timer
-	 
-		PROCESS_WAIT_EVENT();  // Waiting for a event, don't care which
-	 
-		if(etimer_expired(&et)) {  // If the event it's provoked by the timer expiration, then...
-			printf("Timer has expired !");
-		}
-	}	
-	
-	PROCESS_END();
+   static struct etimer et;
+
+  PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
+
+  PROCESS_BEGIN();
+
+  broadcast_open(&broadcast, 129, &broadcast_call);
+
+  while(1) {
+
+    /* Delay 2-4 seconds */
+    etimer_set(&et, CLOCK_SECOND * 4 + random_rand() % (CLOCK_SECOND * 4));
+
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+
+    packetbuf_copyfrom("Hello", 6);
+    broadcast_send(&broadcast);
+    printf("broadcast message sent\n");
+  }
+
+  PROCESS_END();	
 }
