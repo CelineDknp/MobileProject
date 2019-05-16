acf4c349d2408df4ce0c48972c3cfd55cf758358
CelineDknp <celine.deknop@student.uclouvain.be>
2019-05-16 18:03:32 +0200

    Made things clearer

--- test.c
+++ test.c
@@ -15,8 +15,20 @@
 #include "random.h"
 
 enum PACKET_TYPE {
-	ROUTING = 1,
-	DATA = 2
+	ROUTING = 1, //Routing messages to create the tree
+	DATA = 2, //Data messages that go towards the root
+	CMD = 3 //Command messages from the gateway
+};
+
+enum SENDING_MODE { //Mode of sending the data
+	PERIOD = 1, //Send every X seconds
+	CHANGE = 2 //Send only when there is a change
+};
+
+enum TIMINGS {
+	PARENT = 15, //Time after which parent is considered disconnected
+	SEND_ROUTING = 5, //Time frame to send keep alive messages
+	SEND_DATA = 10, //Time frame to send data periodically
 };
 
 typedef struct routing_packet{
@@ -40,6 +52,7 @@ AUTOSTART_PROCESSES(&node_routing_send_process, &node_routing_check_process, &no
 uint8_t rank = 0;
 uint8_t parent_id[2];
 rimeaddr_t parent_addr;
+uint8_t sending_mode = PERIOD; //Start of with sending mode periodically
 static struct etimer parent_timer;
 
 //Functions
@@ -56,24 +69,21 @@ static struct runicast_conn runicast;
 //Processes
 PROCESS_THREAD(node_routing_check_process, ev, data)
 {
-	static struct etimer et;
+	static struct etimer et; //Timer needed to awake process
   	PROCESS_BEGIN();
-	
-	
-	/* Delay 6 secondes */
-    	etimer_set(&parent_timer, CLOCK_SECOND * 15);
+    	etimer_set(&parent_timer, CLOCK_SECOND * PARENT); //After PARENT seconds, we will consider the parent disconnected
 
   	while(1) {
-
 		etimer_set(&et, CLOCK_SECOND);
     		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
     		
-    		if(etimer_expired(&parent_timer) != 0 && rank != 0){ //If timer has expired
+    		if(etimer_expired(&parent_timer) != 0 && rank != 0){ //If timer has expired and I had a parent
 			etimer_reset(&parent_timer);
 			rank = 0;
 			printf("I lost my parent !\n");
 		}	
   	}
+
   	PROCESS_END();	
 }
 
@@ -84,40 +94,43 @@ PROCESS_THREAD(node_routing_send_process, ev, data)
   	PROCESS_BEGIN();
 	
   	broadcast_open(&broadcast, 129, &broadcast_call);
-	
-	/* Delay 3 secondes */
-    	etimer_set(&broad_delay, CLOCK_SECOND * 3);
+    	etimer_set(&broad_delay, CLOCK_SECOND * SEND_ROUTING); //Timer for sending routing infos
 
   	while(1) {
-
     		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&broad_delay));
-    	
     		if(rank != 0){ //If we have a parent
 			send_routing_infos();
 		}
 		etimer_reset(&broad_delay);
   	}
+
   	PROCESS_END();	
 }
 
 PROCESS_THREAD(node_data_process, ev, data)
 {   
 	static struct etimer et;
-
   	PROCESS_EXITHANDLER(runicast_close(&runicast);)
   	PROCESS_BEGIN();
+
   	runicast_open(&runicast, 144, &runicast_call);
+	etimer_set(&et, CLOCK_SECOND * SEND_DATA);
 
   	while(1) {
-
-    	/* Delay 2-4 seconds */
-    	etimer_set(&et, CLOCK_SECOND * 4 + random_rand() % (CLOCK_SECOND * 4));
-
  	   	PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
     	
- 	   	if(rank != 0){ //If rank != 0, has a parent and can send data
-			create_data_packet();
+ 	   	if(rank != 0){ //If rank != 0, has a parent
+			if(sending_mode == PERIOD){// If  sending data periodically, send
+				create_data_packet();
+			}
+			else if(sending_mode == CHANGE){ //If we should only send when there is a change
+				uint8_t change = rand() % 18; //Randomly pick if there is a change
+				if(change % 2 == 0){
+					create_data_packet();
+				}
+			}
  	   	}
+		etimer_reset(&et);
   	}
   	PROCESS_END();	
 }
@@ -125,11 +138,11 @@ PROCESS_THREAD(node_data_process, ev, data)
 // Functions
 static void send_routing_infos(){
 	uint8_t type = ROUTING;
-    routing_packet p = {type, rank};
+    	routing_packet p = {type, rank}; //Create routing packet
 
-    packetbuf_copyfrom(&p, sizeof(routing_packet));
-    broadcast_send(&broadcast);
-    printf("broadcast routing message sent\n"); //Send routing infos
+    	packetbuf_copyfrom(&p, sizeof(routing_packet));
+    	broadcast_send(&broadcast);
+    	printf("broadcast routing message sent\n"); //Send routing infos
 }
 
 static void create_data_packet(){
@@ -147,21 +160,26 @@ static void broadcast_recv(struct broadcast_conn *c, const rimeaddr_t *from)
 {
 	routing_packet p;
 	memcpy(&p, packetbuf_dataptr(), sizeof(routing_packet));
+	
 	printf("broadcast message of type %d received from %d.%d at rank %d\n",  p.message_type,
          from->u8[0], from->u8[1], p.rank);
 	printf("my rank is %d\n", rank);
-    etimer_restart(&parent_timer);
+    	
 	if(rank == 0 || p.rank+1 < rank){ //if my rank is 0, I need a parent
-		rank = p.rank+1;
-		parent_id[0] = from->u8[0];
+		rank = p.rank+1; //Update my rank
+		parent_id[0] = from->u8[0]; //Save parent infos
 		parent_id[1] = from->u8[1];
 		memcpy(&parent_addr, from, sizeof(rimeaddr_t));
+		
 		printf("I found a new parent !\n");
   		printf("my rank is now %d\n", rank);
 		
-		//Found a new parent, broadcast it !
-		send_routing_infos();
+		send_routing_infos(); //Found a new parent, broadcast it !
+		etimer_restart(&parent_timer); //Found a parent, reset the timer
   	}
+	else if(p.rank+1 == rank && parent_id[0] == from->u8[0] && parent_id[1] == from->u8[1]){ //If it was my parent
+		etimer_restart(&parent_timer); //Reset the timer
+	}
 }
 static void runicast_recv(struct runicast_conn *c, const rimeaddr_t *from, uint8_t seqnbr)
 {
@@ -169,7 +187,11 @@ static void runicast_recv(struct runicast_conn *c, const rimeaddr_t *from, uint8
 	memcpy(&p, packetbuf_dataptr(), sizeof(data_packet));
 	printf("unicast message of type %d received from %d.%d saying %d\n",  p.message_type,
          from->u8[0], from->u8[1], p.sensor_data);
-	packetbuf_copyfrom(&p, sizeof(data_packet));
-	runicast_send(&runicast, &parent_addr, 0);
-	printf("unicast message passed along\n");
+	if(rank != 0){ //If I have a parent
+		packetbuf_copyfrom(&p, sizeof(data_packet)); //Need to pass message along
+		runicast_send(&runicast, &parent_addr, 0);
+		printf("unicast message passed along\n");
+	}else{
+		printf("ERROR : Got a unicast message but have no parent to give it to\n");
+	}
 }
